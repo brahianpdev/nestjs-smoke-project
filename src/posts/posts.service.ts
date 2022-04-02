cafb51e2588346d105b317bc3e937d2ec4074b56
brahianpdev <zk.spywn@gmail.com>
2022-04-02 19:48:42 -0300

    Update: Relationships of authors - posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,10 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import UserEntity from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { UpdatePostDto } from './dtos/update-post.dto';
 import PostEntity from './entities/post.entity';
-import { Post } from './interfaces/post.interface';
+import { PostNotFoundException } from './exceptions/postNotFund.exception';
 
 @Injectable()
 export default class PostsService {
@@ -13,34 +14,38 @@ export default class PostsService {
     private postsRepository: Repository<PostEntity>,
   ) {}
 
-  private lastPostId = 0;
-  private posts: Post[] = [];
-
   getAllPosts() {
-    return this.postsRepository.find();
+    return this.postsRepository.find({ relations: ['author'] });
   }
 
   async getPostById(id: number) {
-    const post = await this.postsRepository.findOne(id);
+    const post = await this.postsRepository.findOne(id, {
+      relations: ['author'],
+    });
     if (post) {
       return post;
     }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    throw new PostNotFoundException(id);
   }
 
-  async createPost(post: CreatePostDto) {
-    const newPost = this.postsRepository.create(post);
+  async createPost(post: CreatePostDto, user: UserEntity) {
+    const newPost = this.postsRepository.create({
+      ...post,
+      author: user,
+    });
     await this.postsRepository.save(newPost);
     return newPost;
   }
 
   async updatePost(id: number, post: UpdatePostDto) {
     await this.postsRepository.update(id, post);
-    const updatedPost = await this.postsRepository.findOne(id);
+    const updatedPost = await this.postsRepository.findOne(id, {
+      relations: ['author'],
+    });
     if (updatedPost) {
       return updatedPost;
     }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    throw new PostNotFoundException(id);
   }
 
   async deletePost(id: number) {
