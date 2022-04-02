ea28d31cfa55548abe451dbf3395552ccc2712ff
brahianpdev <zk.spywn@gmail.com>
2022-04-02 19:48:26 -0300

    Update: Add and delete Avatar of User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,21 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import CreateUserDto from './dtos/create-user.dto';
 import UserEntity from './entities/user.entity';
+import { FilesService } from '../files/files.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
+    private readonly filesService: FilesService,
   ) {}
+
+  async create(userData: CreateUserDto) {
+    const newUser = this.usersRepository.create(userData);
+    await this.usersRepository.save(newUser);
+    return newUser;
+  }
 
   async getByEmail(email: string) {
     const user = await this.usersRepository.findOne({ email });
@@ -33,9 +41,28 @@ export class UsersService {
     );
   }
 
-  async create(userData: CreateUserDto) {
-    const newUser = this.usersRepository.create(userData);
-    await this.usersRepository.save(newUser);
-    return newUser;
+  async addAvatar(userId: number, imageBuffer: Buffer, filename: string) {
+    const avatar = await this.filesService.uploadPublicFile(
+      imageBuffer,
+      filename,
+    );
+    const user = await this.getById(userId);
+    await this.usersRepository.update(userId, {
+      ...user,
+      avatar,
+    });
+    return avatar;
+  }
+
+  async deleteAvatar(userId: number) {
+    const user = await this.getById(userId);
+    const fileId = user.avatar?.id;
+    if (fileId) {
+      await this.usersRepository.update(userId, {
+        ...user,
+        avatar: null,
+      });
+      await this.filesService.deletePublicFile(fileId);
+    }
   }
 }
