bb4f7ba8154875e3c5a1e0936d4749d24dcb4107
brahianpdev <zk.spywn@gmail.com>
2022-04-02 19:46:17 -0300

    Config: Set config of aws in main of this app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ExceptionsLoggerFilter } from './utils/excepcionesLogger.filter';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { config } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +13,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ExceptionsLoggerFilter(httpAdapter));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
   app.use(cookieParser());
+
+  const configService = app.get(ConfigService);
+  config.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
